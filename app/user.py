cb86a0684b5d2cbb5bf8a36ebeb06f9addcf48bc
Fabrizio Messina <zauddelig@gmail.com>
2020-11-03 16:03:49 +0100

    updated user class to use mongo

--- app/user.py
+++ app/user.py
@@ -1,28 +1,27 @@
 """
 I was requested to created an authenticator mechanism.
-For business reasons I'm bound to use an sqlite3 database.
+For business reasons I'm bound to use a mongodb database.
 
 Here is what I come up, can you help me find the defects and test this class?
 """
-import sqlite3
+from pymongo.database import Database
 
 
 class Users:
     """ This class will create a database connection. """
-    CREATE = "CREATE TABLE users (username test, password text);"
-    INSERT = "INSERT INTO users VALUES (?, ?);"
-    GET_PASSWORD = "SELECT password WHERE username=? from users;"
 
-    def __init__(self, connection: sqlite3.Connection):
-        self.cursor = connection.cursor()
+    def __init__(self, database: Database):
+        self.collection = database.users
 
     def _get_password(self, username):
-        self.cursor.execute(
-            self.GET_PASSWORD, username)
-        return self.cursor.fetchone()
+        return self.collection.find_one({'username': username})['password']
 
     def authenticate(self, username: str, password: str):
-        return self._get_password(username) == password
+        user_password = self._get_password(username)
+        return user_password == password
 
     def insert_user(self, username: str, password: str):
-        self.cursor.execute(self.INSERT, [password, username])
+        self.collection.insert_one({
+            'password': password,
+            'username': username
+        })
